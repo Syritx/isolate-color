e90b21ca39b92ac68bd27aa8bbcab110cd896e91
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-07-25 10:14:50 -0400

    added source

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+public class Main {
+
+    static String fileName = "Hello.png";
+    static String fileFormat = "png";
+
+    public static void main(String[] args) {
+        BufferedImage image = null;
+        File file = null;
+
+        try {
+            file = new File(fileName);
+            image = ImageIO.read(file);
+        }catch (IOException e) {}
+
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+
+        for (int x = 0; x < imageWidth; x++) {
+            for (int y = 0; y < imageHeight; y++) {
+
+                int rgb = image.getRGB(x,y);
+
+                int alpha = (rgb>>24)&0xff;
+                int red = (rgb>>16)&0xff;
+                int green = (rgb>>8)&0xff;
+                int blue = rgb&0xff;
+
+                int r = 255,g = 255, b = 255;
+
+                if (red == r && green == g && blue == b) {
+                    rgb = (alpha<<24) | (red<<16) | (green<<8) | blue;
+                    image.setRGB(x, y, rgb);
+                }
+                else {
+                    rgb = (alpha<<24) | (0<<16) | (0<<8) | 0;
+                    image.setRGB(x, y, rgb);
+                }
+            }
+        }
+
+        try {
+            file = new File("IsolatedImage." + fileFormat);
+            file.createNewFile();
+
+            ImageIO.write(image,fileFormat,file);
+        }catch(IOException e) {}
+    }
+}
